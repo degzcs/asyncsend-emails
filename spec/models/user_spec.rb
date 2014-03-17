e4e07e0eb41f0d9b70ac74e5935b57c90ae7c30e
Diego Gomez <diego.gomez@codescrum.com>
2014-03-17 12:59:37 -0500

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,7 @@ describe User do
   context 'factory should work' do
     subject { build(:user) }
     its(:name) { should be_present }
-    its(:address) { should be_present }
+    its(:email) { should be_present }
     ##
   end
 
